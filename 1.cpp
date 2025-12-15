33c56bccc93e166f539fc31e600b6de1ddbde95c
SHIVAM GUPTA <shivamgupta.gdscmmmut@gmail.com>
2025-12-15 18:39:32 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /*
+     int a=10;
+    cout << a;
+    cout << "a";
+    cout << "ab" << a;
+    cout<<" enter number";
+    int b,c,d ;
+    cin >>a >>b >>c;
+    cout<< "average is " << (a+b+c)/2; 
+
+
+
+for (int i=0;i<11;i++){
+    int c=0;
+    c= c + i ;
+    cout << c;
+}
+
+int d = 0;
+for (int i=0;i<22;i=i+2){
+    d= d + i ;
+    cout<< d <<endl ;
+   
+}
+cout << d ;
+
+
+
+int d = 20;
+for (int i=20;i<40;i++){
+    if(i%2==0){
+
+    d= d + i ;
+    cout<< d <<endl ;
+   
+}
+}
+cout <<d;
+cout<<i;
+
+
+
+ */
+char ch = 'b';
+cout<< ch;
+
+}
