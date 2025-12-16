5ee9c18710892beac1fb1a097af5e1543b1355f5
SHIVAM GUPTA <shivamgupta.gdscmmmut@gmail.com>
2025-12-16 19:41:36 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+/*
+    // 1.
+char d  ;
+cout<<"DATA ";
+cin >>  d ;
+cout << d <<endl;
+int a= static_cast<int>(d) ;
+
+if (a>=97 && a<=122){
+    cout<<"lowercase";
+}
+else if (a>=65 && a<=90){
+    cout<<"uppercase";
+}
+else if (a>=48 && a<=97){
+    cout<<"numeric";
+}
