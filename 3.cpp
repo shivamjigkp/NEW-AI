d77aed466fca7e36200f852109fec2d269912d32
SHIVAM GUPTA <shivamgupta.gdscmmmut@gmail.com>
2025-12-16 19:42:04 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+   /* int n;
+    cout <<"enter numr";
+    cin >> n;
+
+    int i=1;
+    int j;
+    while(i<=n){
+        int j=1;
+
+        while(j<=n){
+            
+            cout<< n-j+1;
+            j=j+1;
+        }
+
+        cout<< endl;
+        i =i+1;
+
+    }
+
+    
+
+    int n;
+    cout <<"enter numr";
+    cin >> n;
+
+    int i=1;
+    int j;
+    while(i<=n){
+        int j=1;
+
+        while(j<=i){
+            
+            cout<< "*";
+            j=j+1;
+        }
+
+        cout<< endl;
+        i =i+1;
+
+    }
+     
+
+     int n;
+    cout <<"enter numr";
+    cin >> n;
+
+    int i=1;
+    int j;
+    while(i<=n){
+        int j=1;
+
+        while(j<=n-i+1){
+            
+            cout<< "*";
+            j=j+1;
+        }
+
+        cout<< endl;
+        i =i+1;
+
+    }
+
+
+
+    int n;
+    cout <<"enter numr";
+    cin >> n;
+
+    int i=1;
+    int j;
+    while(i<=n){
+        int j=1;
+
+        while(j<=i){
+            char ch =i+j+'A'-2;
+            cout<< ch;
+            j=j+1;
+        }
+
+        cout<< endl;
+        i =i+1;
+
+    }
+*/
+ int n;
+    cout <<"enter numr";
+    cin >> n;
+
+    int i=1;
+    int j;
+    char ch ='A';
+    while(i<=n){
+        int j=1;
+        
+
+        while(j<=i){
+            
+            cout<< ch;
+            ch++ ;
+            j=j+1;
+        }
+
+        cout<< endl;
+        
+        
+        i =i+1;
+
+    }
+
+
+}
