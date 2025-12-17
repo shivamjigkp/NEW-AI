1d8515fa842ac1e794408fde74157ac51d0359ac
SHIVAM GUPTA <shivamgupta.gdscmmmut@gmail.com>
2025-12-17 16:32:31 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        int siz=nums.size();
+        vector<int>p[];
+        vector<int>n[];
+        
+    for(intk=0;k<=siz;k++){
+        if(nums[k]>=0){
+            p.push_back(nums[k];)
+        }
+        else{
+        n.push_back(nums[k];)
+        }
+    }
+
+    if(n.size()==0){
+        for(inti=0;i<p.size();i++){
+             p[i]==p[i]*p[i];
+             return p;
+        }
+    }
+    if(p.size()==0){
+        for(inti;i<n.size();i++){
+             n[i]==n[i]n[i];
+             reverse(n.begin(,n.end()))
+             return n;
+             
+        }
+    }
+
+    
+        while( ){
+
+
+
+
+
+
+
+
+        }
+
+
+      return res[];  
+    }
+};
